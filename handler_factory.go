b4d8c17296a4f865b833b3761cf79e60dce73cd1
Marc Ruiz <marc.ruiz@stayforlong.com>
2022-06-20 12:49:18 +0200

    PLAT-894 remove opencensus handler factory

--- handler_factory.go
+++ handler_factory.go
@@ -6,7 +6,6 @@ import (
 	ginjose "github.com/devopsfaith/krakend-jose/gin"
 	lua "github.com/devopsfaith/krakend-lua/router/gin"
 	metrics "github.com/devopsfaith/krakend-metrics/gin"
-	opencensus "github.com/devopsfaith/krakend-opencensus/router/gin"
 	juju "github.com/devopsfaith/krakend-ratelimit/juju/router/gin"
 	"github.com/luraproject/lura/logging"
 	router "github.com/luraproject/lura/router/gin"
@@ -18,7 +17,6 @@ func NewHandlerFactory(logger logging.Logger, metricCollector *metrics.Metrics, 
 	handlerFactory = lua.HandlerFactory(logger, handlerFactory)
 	handlerFactory = ginjose.HandlerFactory(handlerFactory, logger, rejecter)
 	handlerFactory = metricCollector.NewHTTPHandlerFactory(handlerFactory)
-	handlerFactory = opencensus.New(handlerFactory)
 	handlerFactory = botdetector.New(handlerFactory, logger)
 	return handlerFactory
 }
